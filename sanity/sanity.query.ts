d26c38bf4a4d0d901a3cec0a651e1a271fea77e5
Jonatas Rodolfo Pereira dos Santos <rodolfojonatas@outlook.com>
2023-09-18 23:00:14 -0300

    feat: create a project-slug page

--- sanity/sanity.query.ts
+++ sanity/sanity.query.ts
@@ -2,8 +2,8 @@ import { groq } from "next-sanity";
 import client from "./sanity.client";
 
 export async function getProfile() {
-    return client.fetch(
-        groq`*[_type == "profile"]{
+  return client.fetch(
+    groq`*[_type == "profile"]{
             _id,
             fullName,
             headline,
@@ -16,12 +16,12 @@ export async function getProfile() {
             socialLinks,
             skills
           }`
-    );
+  );
 }
 
 export async function getJob() {
-    return client.fetch(
-        groq`*[_type == "job"]{
+  return client.fetch(
+    groq`*[_type == "job"]{
         _id,
         name,
         jobTitle,
@@ -31,17 +31,31 @@ export async function getJob() {
         startDate,
         endDate,
       }`
-    );
+  );
 }
 
 export async function getProjects() {
-    return client.fetch(
-      groq`*[_type == "project"]{
+  return client.fetch(
+    groq`*[_type == "project"]{
         _id, 
         name,
         "slug": slug.current,
         tagline,
         "logo": logo.asset->url,
       }`
-    );
-  }
+  );
+}
+
+export async function getSingleProject(slug: string) {
+  return client.fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+        _id,
+        name,
+        projectUrl,
+        coverImage { alt, "image": asset->url },
+        tagline,
+        description
+      }`,
+    { slug }
+  );
+}
