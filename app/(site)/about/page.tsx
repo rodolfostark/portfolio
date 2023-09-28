e429eb48991e32aa01158d18ef7048ce2fd12e5b
Jonatas Rodolfo Pereira dos Santos <rodolfojonatas@outlook.com>
2023-09-27 21:08:31 -0300

    style: altering align of e-mail and resume hover

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -37,7 +37,7 @@ export default async function About() {
 
                   <a
                     href={`${data.resumeURL}?dl=${data.fullName}_resume.pdf`}
-                    className="flex items-center justify-center gap-x-2 bg-[#1d1d20] border border-transparent hover:border-zinc-700 rounded-md duration-200 py-2 text-center cursor-cell font-medium"
+                    className="flex items-center justify-center gap-x-2 bg-[#1d1d20] border border-transparent  hover:text-purple-400 hover:border-zinc-700 rounded-md duration-300 py-2 text-center cursor-pointer font-medium"
                   >
                     <BiFile className="text-base" /> Download Resumé
                   </a>
@@ -47,10 +47,10 @@ export default async function About() {
                   <li>
                     <a
                       href={`mailto:${data.email}`}
-                      className="flex items-center gap-x-2 hover:text-purple-400 duration-300"
+                      className="flex items-center justify-center gap-x-2 hover:text-purple-400 duration-300"
                     >
                       <BiEnvelope className="text-lg" />
-                      {data.email}
+                      { data.email }
                     </a>
                   </li>
                 </ul>
