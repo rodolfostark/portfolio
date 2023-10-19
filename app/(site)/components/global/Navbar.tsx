14914180e907386e06c3f9827333be5a8f5c1fe6
Jonatas Rodolfo Pereira dos Santos <rodolfojonatas@outlook.com>
2023-10-19 15:27:41 -0300

    style: update navbar logo

--- app/(site)/components/global/Navbar.tsx
+++ app/(site)/components/global/Navbar.tsx
@@ -1,13 +1,27 @@
-import Image from "next/image";
 import Link from "next/link";
-import Logo from "../../icons/logo.png";
 
 export default function Navbar() {
   return (
     <header className="py-6 md:px-16 px-6 border-b border-zinc-800 z-30 md:mb-28 mb-20">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
-        <Link href="/">
-          <Image src={Logo} width={25} height={25} alt="logo" />
+        <Link
+          href="/"
+          className="hover:text-purple-400 duration-300"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 25 25"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-7 h-7"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
+            />
+          </svg>
         </Link>
         <nav>
           <ul className="flex items-center gap-x-8">
